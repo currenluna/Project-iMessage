cae66d7000f4376faf8b2d08b0b03538bf897df1
currentaber <currentaber@gmail.com>
2023-03-10 11:13:14 -0800

    fully functional!

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 // Make a new iMessage bubble with correct color
-const createMessage = (inText) => {
+const sendMessage = () => {
+    // Get DOM elements
     const newMessage = document.createElement('p');
     const messages = document.getElementsByClassName('imessage')[0];
+    const text = document.getElementById('input-text');
     const checkbox = document.getElementById('input-checkbox');
-    
+
+    // Set text on new message
+    newMessage.innerText = text.value;
+
+    // Determine type of message
     if (checkbox.checked) {
         newMessage.className = 'from-me';
     } else {
         newMessage.className = 'from-them'
     }
     
-    newMessage.innerText = inText;
+    // Add the message to the div
     messages.appendChild(newMessage);
+
+    // Clear the input field
+    text.value = '';
+    console.log('here', text);
 };
-
-
-// When the send button is clicked,
-// Take text from the input text field,
-// Create a new iMessage bubble with the correct color
-const sendMessage = () => {
-    const inputText = document.getElementsByClassName('input-text');
-
-};
-
-createMessage("hi", "hi");
-console.log("hi");
